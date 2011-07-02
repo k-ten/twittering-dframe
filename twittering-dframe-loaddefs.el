9a355d136cc8aa5143e74afa3763b61b4387deb9
Hironori OKAMOTO <k.ten87@gmail.com>
2011-07-03 01:49:24 +0900

    loaddefs を削除。

--- twittering-dframe-loaddefs.el
+++
@@ -1,24 +0,0 @@
-;;; twittering-dframe-loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (twittering-dframe-frame-mode) "twittering-dframe"
-;;;;;;  "twittering-dframe.el" "c67a6c378d20ce29a5d246d31edb3c2d")
-;;; Generated autoloads from twittering-dframe.el
-
-(autoload 'twittering-dframe-frame-mode "twittering-dframe" "\
-Not documented
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-(provide 'twittering-dframe-loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; twittering-dframe-loaddefs.el ends here
